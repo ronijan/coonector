cc340c1918708543e7d915c0f81f4a4a892a9458
Huseyin <55514845+ronijan@users.noreply.github.com>
2024-02-05 10:10:07 +0100

    Update DbOperations.php

--- src/DbOperations.php
+++ src/DbOperations.php
@@ -12,18 +12,52 @@ final class DbOperations
 
     public function __construct(
         private readonly PDO $pdo,
-        private readonly string $targettargetTable)
-    {
-        $this->table = $targettargetTable;
+        private readonly string $targetTable
+    ) {
+        $this->table = $targetTable;
     }
 
-    public function findById(int $id): mixed
+    public function find(int $id): mixed
     {
         $query = $this->pdo->prepare("SELECT * FROM `$this->table` WHERE id = :id");
 
         $query->execute(['id' => $id]);
 
         return $query->fetch();
+    }
+
+    public function findOneBy(array $criteria, array $orderBy = null): mixed
+    {
+        $criteriaAsString = '';
+
+        foreach ($criteria as $key => $value) {
+            $criteriaAsString .= $key . '="' . $value . '" AND ';
+        }
+
+        $criteriaAsStringResult = rtrim($criteriaAsString, " AND ");
+
+        $query = $this->pdo->prepare("SELECT * FROM `$this->table` WHERE $criteriaAsStringResult;");
+
+        $query->execute();
+
+        return $query->fetch();
+    }
+
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null): mixed
+    {
+        $criteriaAsString = '';
+
+        foreach ($criteria as $key => $value) {
+            $criteriaAsString .= $key . '="' . $value . '" AND ';
+        }
+
+        $criteriaAsStringResult = rtrim($criteriaAsString, " AND ");
+
+        $query = $this->pdo->prepare("SELECT * FROM `$this->table` WHERE $criteriaAsStringResult;");
+
+        $query->execute();
+
+        return $query->fetchAll();
     }
 
     public function findAll(): array
@@ -35,7 +69,41 @@ final class DbOperations
         return $query->fetchAll();
     }
 
-    public function deleteById(int $id): bool
+    public function query(string $query): mixed
+    {
+        $query = $this->pdo->prepare("$query");
+
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public function save(array $criteria): bool
+    {
+        $keysAsString = '';
+
+        foreach (array_keys($criteria) as $key) {
+            $keysAsString .= '`' . $key . '`, ';
+        }
+
+        $keys = rtrim($keysAsString, ', ');
+
+        $valuesAsString = '';
+
+        foreach (array_values($criteria) as $value) {
+            $valuesAsString .=  '"' . $value . '", ';
+        }
+
+        $values = rtrim($valuesAsString, ', ');
+
+        $query = $this->pdo->prepare("INSERT INTO `$this->table`($keys) VALUES($values);");
+
+        $query->execute();
+
+        return $query->rowCount() === 1;
+    }
+
+    public function remove(int $id): bool
     {
         $query = $this->pdo->prepare("DELETE FROM `$this->table` WHERE id = :id");
 
